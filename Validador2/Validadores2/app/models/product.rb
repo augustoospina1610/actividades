6a9d677556c71fab6aca1d3a1ee8f96375be2a96
augustoospina1610 <caor1993.b787@gmail.com>
2016-07-21 16:13:47 -0500

    Novedad en product.rb

--- Validador2/Validadores2/app/models/product.rb
+++ Validador2/Validadores2/app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-	validates :name, presence: true, length: {minimum: 5,
-		maximum: 50}
 	validates :marca, :referencia, :costo, presence:true
 	validates :talla, exclusion: {in: %w(s m), message:"%{value} no manejamos esta talla"}
 	validates :color, inclusion: { in: %w(Rojo Verde Azul Amarillo Blanco),
