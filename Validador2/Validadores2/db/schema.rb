a59256881f444ff4866ade1d9da9c7a59468b614
augustoospina1610 <caor1993.b787@gmail.com>
2016-07-21 17:23:30 -0500

    Archivos que se modificaron en Rails

--- Validador2/Validadores2/db/schema.rb
+++ Validador2/Validadores2/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718201551) do
+ActiveRecord::Schema.define(version: 20160721213434) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer  "category_id"
+    t.integer  "product_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_category_products_on_category_id"
+    t.index ["product_id"], name: "index_category_products_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "marca"
